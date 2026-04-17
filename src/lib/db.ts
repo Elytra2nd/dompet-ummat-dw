edfe00c938704e48774c08f795c512d7998149ab
M. Imam Firdaus <mimamfirdaus5@gmail.com>
2026-04-17 11:03:14 +0700

    fix database connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,16 +1,16 @@
 import mariadb from 'mariadb';
 
 export const db = mariadb.createPool({
-  host: process.env.TIDB_HOST,
-  user: process.env.TIDB_USER,
-  password: process.env.TIDB_PASSWORD,
-  database: process.env.TIDB_DATABASE,
-  port: Number(process.env.TIDB_PORT) || 4000,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT) || 4000,
   ssl: {
-    minVersion: 'TLSv1.2',
     rejectUnauthorized: true,
   },
   connectionLimit: 10,
   acquireTimeout: 30000,
   idleTimeout: 60000,
+  connectTimeout: 10000,
 });
